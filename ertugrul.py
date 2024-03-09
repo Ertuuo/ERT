afb084e379cfeeff0c47bb7f6c0ba43b3dc4b76f
Ertuuo <157130537+Ertuuo@users.noreply.github.com>
2024-03-09 12:08:02 +0300

    Update ertugrul.py

--- ertugrul.py
+++ ertugrul.py
@@ -1,1 +1,20 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ah! Sen Teknoloji Bağımlısımıydın?</title>
+    <link rel="stylesheet" href="style.css">
+<body>
+    <h1>TEKNOLOJİ BAĞIMLILARIN DİKKATİNE!!</h1>
+    <p>Selam bağımlı! Hadi beraber aşağıdaki listeyi okuyalım.</p>
+    <h2>İşte Liste!</h2>
+             <ul>
+                   <li>İnternet ve cihazlarınızda geçirdiğiniz zamanı sınırlayın.</li>
+                   <li>Teknoloji kullanımı dışında çeşitli ilgi alanları ve aktiviteler edinin.</li>
+                   <li>Sosyal medya ve mesajlaşma uygulamalarına olan bağımlılığınızı azaltmak için çevrenizle bağlantı kurmanın yeni yollarını arayın.</li>
+                   <li>Mümkünse, bilgisayar başında geçirdiğiniz çalışma süresini sınırlayın ve mola verin. Bu, gözlerinizdeki stresi azaltacak ve bir sonraki çalışma oturumuna geçmeden zihninizin boşalmasına yardımcı olacaktır.</li>
+                   <li>Mümkün olduğunda, teknolojiden uzak geçireceğiniz zaman dilimleri ayarlayarak rahatlamak ve gevşemek için fırsat yaratın.</li>
+                   <li>Teknoloji daha bilinçli kullanmaya çalışın. Arada bir kendinize şu soruyu sorun: Teknolojiye çok mu bağımlı hale geliyorum?</li>
+             </ul>
+     <img src="https://www.mentalup.net/img/blog/telefon-bagimliligi-1.jpg" alt=Image1 width="400"height="300">
+</body>
+</html>
